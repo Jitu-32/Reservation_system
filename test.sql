e61425ff07b379895593c62c0fadb827e1ab240c
Jitendra Shukla <65219820+Jitu-32@users.noreply.github.com>
2020-11-25 14:42:59 +0530

    Resolved bugs in booknow.php

--- test.sql
+++ test.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 24, 2020 at 12:05 PM
+-- Generation Time: Nov 25, 2020 at 08:37 AM
 -- Server version: 10.4.16-MariaDB
 -- PHP Version: 7.4.12
 
@@ -47,9 +47,17 @@ INSERT INTO `admin` (`username`, `password`) VALUES
 
 CREATE TABLE `bookings` (
   `pnr` int(11) NOT NULL,
-  `Train_no.` int(11) NOT NULL,
-  `date` date NOT NULL
+  `train_no` int(10) NOT NULL,
+  `date` date NOT NULL,
+  `booked_by` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `bookings`
+--
+
+INSERT INTO `bookings` (`pnr`, `train_no`, `date`, `booked_by`) VALUES
+(1, 12919, '2020-11-27', 5);
 
 -- --------------------------------------------------------
 
@@ -62,10 +70,10 @@ CREATE TABLE `ticket` (
   `Name` varchar(20) NOT NULL,
   `Age` int(3) NOT NULL,
   `Gender` varchar(2) NOT NULL,
-  `Coach type` varchar(2) NOT NULL,
-  `Seat type` varchar(2) NOT NULL,
-  `Coach no.` int(3) NOT NULL,
-  `Seat no.` int(3) NOT NULL
+  `Coach_type` varchar(2) NOT NULL,
+  `Seat_type` varchar(2) NOT NULL,
+  `coach_no` int(3) NOT NULL,
+  `seat_no` int(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -75,13 +83,29 @@ CREATE TABLE `ticket` (
 --
 
 CREATE TABLE `trains` (
-  `Train no.` int(10) NOT NULL,
+  `Train_no` int(10) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `src` varchar(20) NOT NULL,
+  `dest` varchar(20) NOT NULL,
   `date` date NOT NULL,
   `sl_seats` int(11) NOT NULL,
   `ac_seats` int(11) NOT NULL,
   `avail_sl` int(11) NOT NULL,
   `avail_ac` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `trains`
+--
+
+INSERT INTO `trains` (`Train_no`, `name`, `src`, `dest`, `date`, `sl_seats`, `ac_seats`, `avail_sl`, `avail_ac`) VALUES
+(4353, 'Amarkantak Express', 'Agra', 'Mathura', '2020-12-04', 18, 24, 18, 24),
+(9872, 'Agartala passenger', 'Agartala', 'Howrah', '2020-12-10', 90, 24, 18, 24),
+(12367, 'BPL-HWH express', 'Ropar', 'Bhopal', '2020-12-11', 90, 288, 90, 288),
+(12919, 'Shatabdi express', 'Bhopal', 'Jabalpur', '2020-11-27', 18, 24, 18, 24),
+(14529, 'Malwa Express', 'Indore', 'Vaishno Devi', '2020-12-12', 180, 120, 180, 120),
+(56123, 'Ayush Express', 'Ayush ka ghar', 'Waapis ghar', '2020-12-10', 18, 24, 18, 24),
+(59022, 'Shaktipunj Express', 'Jabalpur', 'Indore', '2020-12-10', 90, 192, 90, 192);
 
 -- --------------------------------------------------------
 
@@ -93,7 +117,7 @@ CREATE TABLE `user` (
   `User id` int(11) NOT NULL,
   `Username` varchar(10) NOT NULL,
   `password` int(11) NOT NULL,
-  `Credit card` int(12) NOT NULL,
+  `Credit_card` int(12) NOT NULL,
   `Address` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -101,8 +125,14 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`User id`, `Username`, `password`, `Credit card`, `Address`) VALUES
-(1, 'gg', 123, 2222, 'hey');
+INSERT INTO `user` (`User id`, `Username`, `password`, `Credit_card`, `Address`) VALUES
+(1, 'gg', 123, 2222, 'hey'),
+(2, '', 12345678, 1236, '136, Abhilasha nagar'),
+(3, 'jitu', 12345678, 0, 'hyy'),
+(4, 'jj', 9999, 12345678, '1355'),
+(5, 'yogi', 4567, 345678, 'cgh'),
+(6, 'Ayush', 12345678, 1234, 'jj'),
+(7, 'vist', 0, 123456, 'Mp');
 
 --
 -- Indexes for dumped tables
@@ -113,19 +143,21 @@ INSERT INTO `user` (`User id`, `Username`, `password`, `Credit card`, `Address`)
 --
 ALTER TABLE `bookings`
   ADD PRIMARY KEY (`pnr`),
-  ADD KEY `Foreign` (`Train_no.`,`date`);
+  ADD KEY `user` (`booked_by`),
+  ADD KEY `train` (`train_no`,`date`);
 
 --
 -- Indexes for table `ticket`
 --
 ALTER TABLE `ticket`
-  ADD PRIMARY KEY (`Coach no.`,`Seat no.`);
+  ADD PRIMARY KEY (`Coach_type`,`coach_no`,`seat_no`),
+  ADD KEY `pnr` (`PNR`);
 
 --
 -- Indexes for table `trains`
 --
 ALTER TABLE `trains`
-  ADD PRIMARY KEY (`Train no.`,`date`);
+  ADD PRIMARY KEY (`Train_no`,`date`);
 
 --
 -- Indexes for table `user`
@@ -141,13 +173,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `bookings`
 --
 ALTER TABLE `bookings`
-  MODIFY `pnr` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `pnr` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `User id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `User id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Constraints for dumped tables
@@ -157,7 +189,14 @@ ALTER TABLE `user`
 -- Constraints for table `bookings`
 --
 ALTER TABLE `bookings`
-  ADD CONSTRAINT `Foreign` FOREIGN KEY (`Train_no.`,`date`) REFERENCES `trains` (`Train no.`, `date`);
+  ADD CONSTRAINT `train` FOREIGN KEY (`train_no`,`date`) REFERENCES `trains` (`Train_no`, `date`),
+  ADD CONSTRAINT `user` FOREIGN KEY (`booked_by`) REFERENCES `user` (`User id`);
+
+--
+-- Constraints for table `ticket`
+--
+ALTER TABLE `ticket`
+  ADD CONSTRAINT `pnr` FOREIGN KEY (`PNR`) REFERENCES `bookings` (`pnr`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- user/booknow.php
+++ user/booknow.php
@@ -1,6 +1,6 @@
-<?php
+<!-- <?php
     include('header.php');
-?>
+?> -->
     <!-- Above code is same as index.php 
         Starting the code for admin login
     -->
@@ -16,28 +16,28 @@
         $tbl_name="trains"; // Table name
         mysqli_select_db($conn,"$db_name")or die("cannot select DB");
 
-        if('$coach'=="AC"){      // AC Coach
+        if( $coach =="AC"){      // AC Coach
 
-            $sql="SELECT * FROM $tbl_name WHERE Train_num='$train_num' and train_date='$train_date'and avail_ac >= '$passenger_num'";
+            $sql="SELECT * FROM trains WHERE Train_no= '$train_num' and date= '$train_date'and avail_ac >= '$passenger_num'";
             $result=mysqli_query($conn,$sql) or trigger_error(mysql_error.$sql);
             
             if(mysqli_num_rows($result) < 1)
             {
                 echo " .... LOGIN TRY  ....";
                 $_SESSION['error'] = "1";
-                header("location:booknow.php"); 
+                header("location:../index.php"); 
             }
         }
         else{                   // Sleeper Coach
 
-            $sql="SELECT * FROM $tbl_name WHERE Train_num='$train_num' and train_date='$train_date' and avail_sl='$passenger_num' ";
+            $sql="SELECT * FROM trains WHERE Train_no='$train_num' and date='$train_date' and avail_sl >='$passenger_num' ";
             $result=mysqli_query($conn,$sql) or trigger_error(mysql_error.$sql);
             
             if(mysqli_num_rows($result) < 1)
             {
                 echo " .... LOGIN TRY  ....";
                 $_SESSION['error'] = "1";
-                header("location:booknow.php"); 
+                header("location:../index.php"); 
             }
         }
 
@@ -51,35 +51,65 @@
         // Finding the current PNR number.
         $tbl_name="trains";
         mysqli_select_db($conn,"$db_name")or die("cannot select DB");
-        $query = mysqli_query("SELECT MAX(pnr) as max_id FROM '$tbl_name'"); 
-        $row = mysqli_fetch_array($query); 
-        $cur_pnr = $row[‘max_id’];
-        echo $cur_pnr ;
+        $query = "SELECT MAX('pnr') as max_id FROM trains"; 
+        $result= mysqli_query($conn,$query);
+        // $row   =   mysqli_fetch_array($result); 
+        // $cur_pnr = $row["max_id"];
+        $cur_pnr = 0;
+        if ($conn->query($sql) === TRUE) {
+            $last_id = $conn->insert_id;
+            $cur_pnr = $last_id;
+            echo "New record created successfully. Last inserted ID is: " . $last_id;
+          } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+        //echo $cur_pnr ;
 
         $localvar = $passenger_num;
         $tbl_name = "ticket";
         
-        if('$coach'=="AC"){
+        if($coach =="AC"){
             $tbl_name="trains";
             mysqli_select_db($conn,"$db_name")or die("cannot select DB");
-            $query = mysqli_query("SELECT * FROM $tbl_name WHERE train_no =='$train_num' and date == '$train_date'");
-            $row = mysqli_fetch_array($query);
-            $avail_ac = row['avail_ac'];
-            $ac_seats = row['ac_seats'];
+            $query  = "SELECT * FROM trains WHERE train_no ='$train_num' and date = '$train_date'";
+            $result = mysqli_query($conn,$query);
+            // if ($conn->query($sql) === TRUE) {
+            //     $last_id = $conn->insert_id;
+            //     $cur_pnr = $last_id;
+            //     echo "New record created successfully. Last inserted is acccc " . $last_id;
+            //   } else {
+            //     echo "Error: " . $sql . "<br>" . $conn->error;
+            // }
+            $row = mysqli_fetch_array($result);
+            $avail_ac = $row[8];
+            //echo $row["train_no"] . "<br> ";
+            // echo $row[1] . "<br> ";
+            // echo $row[2] . "<br> ";
+            // echo $row[3] . "<br> ";
+            // echo $row[4] . "<br> ";
+            // echo $row[5] . "<br> ";
+            // echo $row[6] . "<br> ";
+            // echo $row[7] . "<br> ";
+            // echo $row[8] . "<br> ";
+            $ac_seats = $row[6];
             $AC_sitting = array("LB","LB","UB","UB","SL","SU");
             $cur_ac = $ac_seats - $avail_ac;
             $net = $avail_ac-$passenger_num;
-            $sql = "UPDATE $tbl_name SET avail_ac=$net WHERE Train_no =='$train_num' and date == '$train_date'";
-            
+            $sql = "UPDATE trains SET avail_ac = '$net' WHERE Train_no ='$train_num' and date = '$train_date'";
+            //$query  = "SELECT * FROM trains WHERE train_no ='$train_num' and date = '$train_date'";
+            $result = mysqli_query($conn,$sql);
+
             if($localvar > 0){       // Passenger 1
                 $name1 = $_POST['name1'];
                 $age1 = $_POST['age1'];
                 $gender1 = $_POST['gender1']; 
-                $temp = $cur_ac%6;
-                $temp1= $temp+1;
-                $temp2 = ($cur_ac+1)/18;
-                $sql = "INSERT INTO $tbl_name (PNR,Name, Age, Gender,Coach_type, seat_type, coach_no, seat_no)
-                VALUES ('$cur_pnr','$name1','$age1','$gender1','AC','AC_sitting[$temp]','$temp2','$temp1')";
+                echo $name1 ;
+                $temp = ($cur_ac )% 6;
+                $temp1= $cur_ac + 1;
+                $temp2 = ($cur_ac)/18 + 1;
+                $sql = "INSERT INTO ticket (PNR,Name, Age, Gender,Coach_type, seat_type, coach_no, seat_no)
+                VALUES ('$cur_pnr','$name1','$age1','$gender1','AC','$AC_sitting[$temp]','$temp2','$temp1')";
+                $result=mysqli_query($conn,$sql);
                 $cur_ac++;
                 $localvar--;
             }
@@ -88,11 +118,12 @@
                 $name2 = $_POST['name2'];
                 $age2 = $_POST['age2'];
                 $gender2 = $_POST['gender2'];
-                $temp = $cur_ac % 6;
-                $temp1= $temp+1;
-                $temp2 = ($cur_ac+1)/18;
-                $sql = "INSERT INTO $tbl_name (PNR,Name, Age, Gender,Coach_type, seat_type, coach_no, seat_no)
-                VALUES ('$cur_pnr','$name2','$age2','$gender2','AC','AC_sitting[$temp]','$temp2','$temp1')";
+                $temp = ($cur_ac )% 6;
+                $temp1= $cur_ac + 1;
+                $temp2 = ($cur_ac)/18 + 1;
+                $sql = "INSERT INTO ticket (PNR,Name, Age, Gender,Coach_type, seat_type, coach_no, seat_no)
+                VALUES ('$cur_pnr','$name2','$age2','$gender2','AC','$AC_sitting[$temp]','$temp2','$temp1')";
+                $result=mysqli_query($conn,$sql);
                 $cur_ac++;
                 $localvar--;
             }
@@ -100,12 +131,13 @@
             if($localvar > 0){       // Passenger 3
                 $name3 = $_POST['name3'];
                 $age3 = $_POST['age3'];
-                $gende3 = $_POST['gender3'];  
-                $temp = $cur_ac%6;
-                $temp1= $temp+1;
-                $temp2 = ($cur_ac+1)/18;
-                $sql = "INSERT INTO $tbl_name (PNR,Name, Age, Gender,Coach_type, seat_type, coach_no, seat_no)
-                VALUES ('$cur_pnr','$name3','$age3','$gender3','AC','AC_sitting[$temp]','$temp2','$temp1')";
+                $gender3 = $_POST['gender3'];  
+                $temp = ($cur_ac )% 6;
+                $temp1= $cur_ac + 1;
+                $temp2 = ($cur_ac)/18 + 1;
+                $sql = "INSERT INTO ticket (PNR,Name, Age, Gender,Coach_type, seat_type, coach_no, seat_no)
+                VALUES ('$cur_pnr','$name3','$age3','$gender3','AC','$AC_sitting[$temp]','$temp2','$temp1')";
+                $result=mysqli_query($conn,$sql);
                 $cur_ac++;
                 $localvar--;
             }
@@ -114,11 +146,12 @@
                 $name4 = $_POST['name4'];
                 $age4 = $_POST['age4'];
                 $gender4 = $_POST['gender4'];  
-                $temp = $cur_ac%6;
-                $temp1= $temp+1;
-                $temp2 = ($cur_ac+1)/18;
-                $sql = "INSERT INTO $tbl_name (PNR,Name, Age, Gender,Coach_type, seat_type, coach_no, seat_no)
-                VALUES ('$cur_pnr','$name4','$age4','$gender4','AC','AC_sitting[$temp]','$temp2','$temp1')";
+                $temp = ($cur_ac )% 6;
+                $temp1= $cur_ac + 1;
+                $temp2 = ($cur_ac)/18 + 1;
+                $sql = "INSERT INTO ticket (PNR,Name, Age, Gender,Coach_type, seat_type, coach_no, seat_no)
+                VALUES ('$cur_pnr','$name4','$age4','$gender4','AC','$AC_sitting[$temp]','$temp2','$temp1')";
+                $result=mysqli_query($conn,$sql);
                 $cur_ac++;
                 $localvar--;
             }
@@ -127,37 +160,42 @@
                 $name5 = $_POST['name5'];
                 $age5 = $_POST['age5'];
                 $gender5 = $_POST['gender5']; 
-                $temp = $cur_ac%6;
-                $temp1= $temp+1;
-                $temp2 = ($cur_ac+1)/18;
-                $sql = "INSERT INTO $tbl_name (PNR,Name, Age, Gender,Coach_type, seat_type, coach_no, seat_no)
-                VALUES ('$cur_pnr','$name5','$age5','$gender5','AC','AC_sitting[$temp]','$temp2','$temp1')";
+                $temp = ($cur_ac )% 6;
+                $temp1= $cur_ac + 1;
+                $temp2 = ($cur_ac)/18 + 1;
+                $sql = "INSERT INTO ticket (PNR,Name, Age, Gender,Coach_type, seat_type, coach_no, seat_no)
+                VALUES ('$cur_pnr','$name5','$age5','$gender5','AC','$AC_sitting[$temp]','$temp2','$temp1')";
+                $result=mysqli_query($conn,$sql);
                 $cur_ac++;
                 $localvar--;
             }
         }
 
-        if('$coach'=="Sleeper"){
+        if($coach == "SL"){
             $tbl_name="trains";
             mysqli_select_db($conn,"$db_name")or die("cannot select DB");
-            $query = mysqli_query("SELECT * FROM $tbl_name WHERE Train_no =='$train_num' and date == '$train_date'");
-            $row = mysqli_fetch_array($query);
-            $avail_sl = row['avail_sl'];
-            $sl_seats = row['sl_seats'];
+            $query = "SELECT * FROM trains WHERE Train_no = '$train_num' and date = '$train_date'";
+            $result=mysqli_query($conn,$query);
+            $row = mysqli_fetch_array($result);
+            $avail_sl = $row[7];
+            $sl_seats = $row[5];
             $SL_sitting = array("LB","MB","UB","LB","MB","UB","SL","SU");
             $cur_sl = $sl_seats - $avail_sl;
             $net = $avail_sl-$passenger_num;
-            $sql = "UPDATE $tbl_name SET avail_sl=$net WHERE Train_no =='$train_num' and date == '$train_date'";
+            $sql = "UPDATE trains SET avail_sl= '$net' WHERE Train_no ='$train_num' and date = '$train_date'";
+            $result = mysqli_query($conn,$sql);
+
 
             if($localvar > 0){       // Passenger 1
                 $name1 = $_POST['name1'];
                 $age1 = $_POST['age1'];
                 $gender1 = $_POST['gender1']; 
-                $temp = $cur_sl%8;
-                $temp1= $temp+1;
-                $temp2 = ($cur_sl+1)/24;
-                $sql = "INSERT INTO $tbl_name (PNR,Name, Age, Gender,Coach_type, seat_type, coach_no, seat_no)
-                VALUES ('$cur_pnr','$name1','$age1','$gender1','SL','SL_sitting[$temp]','$temp2','$temp1')";
+                $temp = ($cur_sl )% 8;
+                $temp1= $cur_sl + 1;
+                $temp2 = ($cur_sl + 1)/24 + 1;
+                $sql = "INSERT INTO ticket (PNR,Name, Age, Gender,Coach_type, seat_type, coach_no, seat_no)
+                VALUES ('$cur_pnr','$name1','$age1','$gender1','SL','$SL_sitting[$temp]','$temp2','$temp1')";
+                $result=mysqli_query($conn,$sql);
                 $cur_sl++;
                 $localvar--;
             }
@@ -166,11 +204,12 @@
                 $name2 = $_POST['name2'];
                 $age2 = $_POST['age2'];
                 $gender2 = $_POST['gender2']; 
-                $temp = $cur_sl%8;
-                $temp1= $temp+1;
-                $temp2 = ($cur_sl+1)/24;
-                $sql = "INSERT INTO $tbl_name (PNR,Name, Age, Gender,Coach_type, seat_type, coach_no, seat_no)
-                VALUES ('$cur_pnr','$name2','$age2','$gender2','SL','SL_sitting[$temp]','$temp2','$temp1')";
+                $temp = ($cur_sl )% 8;
+                $temp1= $cur_sl + 1;
+                $temp2 = ($cur_sl + 1)/24 + 1;
+                $sql = "INSERT INTO ticket (PNR,Name, Age, Gender,Coach_type, seat_type, coach_no, seat_no)
+                VALUES ('$cur_pnr','$name2','$age2','$gender2','SL','$SL_sitting[$temp]','$temp2','$temp1')";
+                $result=mysqli_query($conn,$sql);
                 $cur_sl++;
                 $localvar--;
             }
@@ -178,12 +217,13 @@
             if($localvar > 0){       // Passenger 3
                 $name3 = $_POST['name3'];
                 $age3 = $_POST['age3'];
-                $gende3 = $_POST['gender3']; 
-                $temp = $cur_sl%8;
-                $temp1= $temp+1;
-                $temp2 = ($cur_sl+1)/24;
-                $sql = "INSERT INTO $tbl_name (PNR,Name, Age, Gender,Coach_type, seat_type, coach_no, seat_no)
-                VALUES ('$cur_pnr','$name3','$age3','$gender3','SL','SL_sitting[$temp]','$temp2','$temp1')";
+                $gender3 = $_POST['gender3']; 
+                $temp = ($cur_sl )% 8;
+                $temp1= $cur_sl + 1;
+                $temp2 = ($cur_sl + 1)/24 + 1;
+                $sql = "INSERT INTO ticket (PNR,Name, Age, Gender,Coach_type, seat_type, coach_no, seat_no)
+                VALUES ('$cur_pnr','$name3','$age3','$gender3','SL','$SL_sitting[$temp]','$temp2','$temp1')";
+                $result=mysqli_query($conn,$sql);
                 $cur_sl++;
                 $localvar--;
             }
@@ -192,11 +232,12 @@
                 $name4 = $_POST['name4'];
                 $age4 = $_POST['age4'];
                 $gender4 = $_POST['gender4']; 
-                $temp = $cur_sl%8;
-                $temp1= $temp+1;
-                $temp2 = ($cur_sl+1)/24;
-                $sql = "INSERT INTO $tbl_name (PNR,Name, Age, Gender,Coach_type, seat_type, coach_no, seat_no)
-                VALUES ('$cur_pnr','$name4','$age4','$gender4','SL','SL_sitting[$temp]','$temp2','$temp1')";
+                $temp = ($cur_sl )% 8;
+                $temp1= $cur_sl + 1;
+                $temp2 = ($cur_sl + 1)/24 + 1;
+                $sql = "INSERT INTO ticket (PNR,Name, Age, Gender,Coach_type, seat_type, coach_no, seat_no)
+                VALUES ('$cur_pnr','$name4','$age4','$gender4','SL','$SL_sitting[$temp]','$temp2','$temp1')";
+                $result=mysqli_query($conn,$sql);
                 $cur_sl++;
                 $localvar--;
             }
@@ -205,11 +246,12 @@
                 $name5 = $_POST['name5'];
                 $age5 = $_POST['age5'];
                 $gender5 = $_POST['gender5']; 
-                $temp = $cur_sl%8;
-                $temp1= $temp+1;
-                $temp2 = ($cur_sl+1)/24;
-                $sql = "INSERT INTO $tbl_name (PNR,Name, Age, Gender,Coach_type, seat_type, coach_no, seat_no)
-                VALUES ('$cur_pnr','$name5','$age5','$gender5','SL','SL_sitting[$temp]','$temp2','$temp1')";
+                $temp = ($cur_sl )% 8;
+                $temp1= $cur_sl + 1;
+                $temp2 = ($cur_sl + 1)/24 + 1;
+                $sql = "INSERT INTO ticket (PNR,Name, Age, Gender,Coach_type, seat_type, coach_no, seat_no)
+                VALUES ('$cur_pnr','$name5','$age5','$gender5','SL','$SL_sitting[$temp]','$temp2','$temp1')";
+                $result=mysqli_query($conn,$sql);
                 $cur_sl++;
                 $localvar--;
             }
@@ -228,25 +270,25 @@
         Passenger1 Name : <input type="text" name="name1" placeholder = "Enter name"><br>
         Passenger1 Age : <input type="number" name="age1" placeholder = "Enter Age"><br>
         Passenger1 Gender : <input type="text" name="gender1" placeholder = "Male or Female"><br><br>
-        Passenger2 Name : <input type="text" name="name1" placeholder = "Enter name"><br>
-        Passenger2 Age : <input type="number" name="age1" placeholder = "Enter Age"><br>
-        Passenger2 Gender : <input type="text" name="gender1" placeholder = "Male or Female"><br><br>
-        Passenger3 Name : <input type="text" name="name1" placeholder = "Enter name"><br>
-        Passenger3 Age : <input type="number" name="age1" placeholder = "Enter Age"><br>
-        Passenger3 Gender : <input type="text" name="gender1" placeholder = "Male or Female"><br><br>
-        Passenger4 Name : <input type="text" name="name1" placeholder = "Enter name"><br>
-        Passenger4 Age : <input type="number" name="age1" placeholder = "Enter Age"><br>
-        Passenger4 Gender : <input type="text" name="gender1" placeholder = "Male or Female"><br><br>
-        Passenger5 Name : <input type="text" name="name1" placeholder = "Enter name"><br>
-        Passenger5 Age : <input type="number" name="age1" placeholder = "Enter Age"><br>
-        Passenger5 Gender : <input type="text" name="gender1" placeholder = "Male or Female"><br><br>
+        Passenger2 Name : <input type="text" name="name2" placeholder = "Enter name"><br>
+        Passenger2 Age : <input type="number" name="age2" placeholder = "Enter Age"><br>
+        Passenger2 Gender : <input type="text" name="gender2" placeholder = "Male or Female"><br><br>
+        Passenger3 Name : <input type="text" name="name3" placeholder = "Enter name"><br>
+        Passenger3 Age : <input type="number" name="age3" placeholder = "Enter Age"><br>
+        Passenger3 Gender : <input type="text" name="gender3" placeholder = "Male or Female"><br><br>
+        Passenger4 Name : <input type="text" name="name4" placeholder = "Enter name"><br>
+        Passenger4 Age : <input type="number" name="age4" placeholder = "Enter Age"><br>
+        Passenger4 Gender : <input type="text" name="gender4" placeholder = "Male or Female"><br><br>
+        Passenger5 Name : <input type="text" name="name5" placeholder = "Enter name"><br>
+        Passenger5 Age : <input type="number" name="age5" placeholder = "Enter Age"><br>
+        Passenger5 Gender : <input type="text" name="gender5" placeholder = "Male or Female"><br><br>
         <input type="submit" value="Submit" name="Submit">
     </form>
 
 </div>
 
-
+<!-- 
 <?php
     include('footer.php');
-?>
+?> -->
 
